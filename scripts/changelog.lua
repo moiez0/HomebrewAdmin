f397f9bc45776335f78895375aebdc4d0d6f9454
Muhammad <muhammadmasoom1001@gmail.com>
2021-05-26 20:43:50 +0100

    Update changelog.lua

--- scripts/changelog.lua
+++ scripts/changelog.lua
@@ -1,9 +1,6 @@
 changelog = [[
 Changelog:
--- Made notify function global for plugins
--- Added aliase prefix for prefixchange
--- Improved goto by using any part instead of HRP
--- Changed it so every cmd tries to  unpacks with args even if it doesn't match the cmds number of args.
--- Added changelog
+-- Plugin system completely re-written
+-- Plugins are now pcall'ed
 ]]
 return changelog
