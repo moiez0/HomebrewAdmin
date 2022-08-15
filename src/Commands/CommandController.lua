918aa30a3f6ce315f01b658f14b273fd403b2863
7n7o <7n7o7n7o@gmail.com>
2022-08-15 16:11:51 +0100

    fix command arguments

--- src/Commands/CommandController.lua
+++ src/Commands/CommandController.lua
@@ -18,7 +18,7 @@ function CommandController:Init()
         local Command = CommandExecution:GetCommand()
         local Args = CommandExecution:GetArguments()
         Thread.Spawn(function()
-            Command:Execute(Args)
+            Command:Execute(unpack(Args))
         end)
     end)
 
