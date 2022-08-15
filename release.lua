9248c8629f336e61caa45d42ae0d674b8b59bc34
7n7o <7n7o7n7o@gmail.com>
2022-08-15 14:53:46 +0100

    bfffdsfd

---
+++ release.lua
@@ -0,0 +1,10 @@
+local Rostruct = loadstring(game:HttpGet"https://github.com/richie0866/Rostruct/releases/download/v1.1.11/Rostruct.lua")()
+
+Rostruct
+    -- Download the lastest release to local files
+    .fetchLatest("7n7o", "HomebrewAdmin-Rewritten")
+    -- Then, build and start all scripts
+    :andThen(function(pkg)
+        pkg:build("src/")
+        pkg:start()
+    end)
