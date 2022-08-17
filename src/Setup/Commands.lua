a7f4f49a6d99635a324e5fe6d9c6202fb8f3be0d
7n7o <7n7o7n7o@gmail.com>
2022-08-17 18:34:07 +0100

    dev test

--- src/Setup/Commands.lua
+++ src/Setup/Commands.lua
@@ -70,6 +70,7 @@ function Commands:Init(CommandController)
             antichatreport:SetStore("enabled", not enabled)
         end,
         init = function(self)
+            print(self)
             local chatreport = self
             self:SetStore("enabled", false)
             local Players = game:GetService("Players")
