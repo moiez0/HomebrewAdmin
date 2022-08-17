f6a77b9d1005c6fa26d3030cbafa9c74d0cc996f
Syntax <muhammadmasoom1001@gmail.com>
2022-08-17 13:19:21 +0100

    Update Commands.lua

--- src/Setup/Commands.lua
+++ src/Setup/Commands.lua
@@ -65,6 +65,150 @@ function Commands:Init(CommandController)
     )
 
     AddCommand(
+        "force",
+        "Use the force on target [TOOL REQUIRED]",
+        1,
+        {"hold", "theforce"},
+        function(Target)
+                    
+        
+        local Players = game:GetService("Players")
+        local CoreGui = game:GetService("CoreGui")
+        local RunService = game:GetService("RunService")
+        local ReplicatedStorage = game:GetService("ReplicatedStorage")
+        local UserInputService = game:GetService("UserInputService")
+        local Lighting = game:GetService("Lighting")
+        local StarterGui = game:GetService("StarterGui")
+        
+        local Player = Players.LocalPlayer
+        local Character = Player.Character
+        
+        local PlayerGui = Player:WaitForChild("PlayerGui")
+        local Backpack = Player:WaitForChild("Backpack")
+        
+        local Humanoid = Character and Character:FindFirstChildWhichIsA("Humanoid") or false
+        local RootPart = Character and Humanoid and Humanoid.RootPart or false
+        local RightArm = Character and Character:FindFirstChild("Right Arm") or Character:FindFirstChild("RightHand")
+        if not Humanoid or not RootPart or not RightArm then
+            return
+        end
+        
+        local AnimationIds = {
+            ["Grab"] = {
+                ["R6"] = 182393478,
+                ["R15"] = 393915542
+            }
+        }
+        
+        local CreateRightGrip = function(Tool)
+            if Tool and RightArm then
+                local Handle = Tool and Tool:FindFirstChild("Handle") or false
+                if Handle then
+                    local Weld = Instance.new("Weld")
+                    Weld.Name = "RightGrip"
+                    Weld.Part0 = RightArm
+                    Weld.Part1 = Handle
+                    Weld.C0 = CFrame.new(0, -5, -3) * CFrame.Angles(math.rad(-90), math.rad(0), math.rad(0))--math.rad(-90) 
+                    Weld.C1 = Tool.Grip
+                    Weld.Parent = RightArm
+                    return Weld
+                end
+            end
+        end
+        
+        Humanoid:UnequipTools()
+        local MainTool = Backpack:FindFirstChildWhichIsA("Tool") or false
+        if not MainTool or not MainTool:FindFirstChild("Handle") then
+            return
+        end
+        
+        local TPlayer = GetPlayer(Target)
+        local TCharacter = TPlayer and TPlayer.Character
+        
+        local THumanoid = TCharacter and TCharacter:FindFirstChildWhichIsA("Humanoid") or false
+        local TRootPart = TCharacter and THumanoid and THumanoid.RootPart or false
+        if not THumanoid or not TRootPart then
+            return
+        end
+        
+        if Character:FindFirstChild("Animate") then
+            Character:FindFirstChild("Animate").Disabled = true
+        end
+        for _, x in next, Humanoid:GetPlayingAnimationTracks() do
+            x:Stop()
+        end
+        CreateRightGrip(MainTool)
+        MainTool.Parent = Character
+        MainTool.Handle:BreakJoints()
+        MainTool.Parent = Backpack
+        MainTool.Parent = Humanoid
+        CreateRightGrip(MainTool)
+        if firetouchinterest then
+            firetouchinterest(MainTool.Handle, TRootPart, 0)
+            firetouchinterest(MainTool.Handle, TRootPart, 1)
+        else
+            if Humanoid.RigType == Enum.HumanoidRigType.R6 then
+                local OldCFrame = RootPart.CFrame
+                local OldTick = tick()
+                repeat
+                    task.wait()
+                    RootPart.CFrame = TRootPart.CFrame * CFrame.new(0, 2, 2)
+                    if MainTool.Parent ~= Humanoid then
+                        break
+                    end
+                until (tick() - OldTick) > 3
+                RootPart.CFrame = OldCFrame
+            else
+                return
+            end
+        end
+        
+        THumanoid:SetStateEnabled(Enum.HumanoidStateType.GettingUp, false)
+        THumanoid:ChangeState(Enum.HumanoidStateType.Ragdoll)
+        local MainAnimation = Instance.new("Animation", Humanoid)
+        local Animation
+        local WaitTime
+        if Humanoid.RigType == Enum.HumanoidRigType.R6 then
+            MainAnimation.AnimationId = "rbxassetid://"..AnimationIds["Grab"]["R6"]
+            Animation = Humanoid:LoadAnimation(MainAnimation)
+        else
+            MainAnimation.AnimationId = "rbxassetid://"..AnimationIds["Grab"]["R15"]
+            Animation = Humanoid:LoadAnimation(MainAnimation)
+        end
+        Animation.Looped = false
+        Animation:Play()
+        task.wait(WaitTime)
+        Animation:AdjustSpeed(0)
+        
+        notify("Press K to kill")
+        notify("Press E to drop")
+        
+        local KillTPlayer; KillTPlayer = UserInputService.InputBegan:Connect(function(Input, GameProcessed)
+            if not GameProcessed and Input.KeyCode == Enum.KeyCode.K then
+                KillTPlayer:Disconnect()
+                RootPart.Anchored = true
+                RootPart.Anchored = false
+                Player.Character = nil
+                Humanoid.Health = 0
+                Character:BreakJoints()
+            elseif not GameProcessed and Input.KeyCode == Enum.KeyCode.E then
+                Animation:Stop()
+                KillTPlayer:Disconnect()
+                if Character:FindFirstChild("Animate") then
+                    Character:FindFirstChild("Animate").Disabled = false
+                end
+                if Humanoid.RigType == Enum.HumanoidRigType.R6 then
+                    Character["Right Arm"].RightGrip:Destroy()
+                else
+                    Character["RightHand"].RightGripAttachment:Destroy()
+                    Character["RightHand"].RightGrip:Destroy()
+                end
+            end
+        end)
+       end
+    )
+
+    AddCommand(
         "tfling",
         "makes your tool fling (dont equip)",
         0,
